97170de205077ec007510d793ca97f8e0a99e6dc
antomich3 <62870796+antomich3@users.noreply.github.com>
2025-02-19 12:50:39 +0000

    commit 19.02.2025

--- TextSumchatgpt.py
+++ TextSumchatgpt.py
@@ -3,7 +3,7 @@
 #The user can input messages, which are added to the conversation history. The chatbot then uses the conversation history to generate a response using the GPT-3.5 model.
 #The generated response is added to the conversation history, and the chatbot continues the conversation by prompting the user for another input.
 #The conversation continues in this manner until the user exits the chatbot by not providing any input.
-
+import datetime
 import openai
 
 openai.api_key = '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'
