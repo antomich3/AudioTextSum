a22cd2db3fed7cbcd38b8f9dfdc95f6983542da1
antomich3 <62870796+antomich3@users.noreply.github.com>
2025-02-19 12:52:03 +0000

    commit 19.02.2025

--- TextSumchatgpt.py
+++ TextSumchatgpt.py
@@ -6,7 +6,7 @@
 import datetime
 import openai
 
-openai.api_key = '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'
+openai.api_key = ''
 
 messages = [{"role": "system", "content": "You are an intelligent assistant."}]
 
